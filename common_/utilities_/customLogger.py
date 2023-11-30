4cd5ce8afdcbe4e36bd8c66639951642714c1930
MkhitaryanSatik <mkhitaryansatik@gmail.com>
2023-11-30 17:45:18 +0400

    QAG2-42: modified log message

--- common_/utilities_/customLogger.py
+++ common_/utilities_/customLogger.py
@@ -1,13 +1,14 @@
 import logging
 from datetime import date
 import os
+from pathlib import Path
 
 
 def get_root_dir_name():
     command = "Path(__file__).absolute()"
     while True:
         rootDir = str(eval(command))
-        if rootDir.split(sep="\\")[-1] == "AGBU_AmazonTestEnvironment":
+        if rootDir.split(sep="\\")[-1] == "AmazonTestEnvironment_MkhitaryanSatik":
             return rootDir
         else:
             command += ".parent"
@@ -18,17 +19,17 @@ def logger(level, message, fileName=os.path.join(get_root_dir_name(), '_logs_', 
                         format="%(asctime)-12s %(levelname)s %(message)s",
                         datefmt="%d-%m-%Y %H:%M:%S")
     if level == "INFO":
-        logging.info(message)
-        print(message)
+        logging.info(f"{level}: {message}")
+        print(f"{level}: {message}")
     elif level == "DEBUG":
-        logging.debug(message)
-        print(message)
+        logging.debug(f"{level}: {message}")
+        print(f"{level}: {message}")
     elif level == "WARNING":
-        logging.warning(message)
-        print(message)
+        logging.warning(f"{level}: {message}")
+        print(f"{level}: {message}")
     elif level == "ERROR":
-        logging.error(message)
-        print(message)
+        logging.error(f"{level}: {message}")
+        print(f"{level}: {message}")
     elif level == "CRITICAL":
-        logging.critical(message)
-        print(message)
+        logging.critical(f"{level}: {message}")
+        print(f"{level}: {message}")
